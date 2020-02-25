94ae1020430d33dc823c699a2302458341116a08
Jorge <jorgeruizdevelopers@gmail.com>
2020-02-26 00:20:22 +0100

    navbar y css

--- js/app.js
+++
@@ -1,7 +0,0 @@
-const Search = document.querySelector("#search");
-const text = document.querySelector("#searchText");
-
-Search.addEventListener('click', () => {
-    console.log(text.value);
-});
-
