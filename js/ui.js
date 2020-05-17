c9c04627678a4174d3bedf884a8dd4a416a5cf19
Jorge Ruiz Martínez <jorgeruizdevelopers@gmail.com>
2020-05-17 17:55:42 +0200

    tslint

--- js/ui.js
+++ js/ui.js
@@ -1,13 +1,13 @@
 
 // Recibimos un elemento y luego retornamos otra funcion con las clases
 // Función para añadir y quitar clases de elementos.
- export const toogle = elemento => (removeClass, addClass) => {
-    elemento.classList.remove(removeClass);
-    elemento.classList.add(addClass);
+export const toogle = elemento => (removeClass, addClass) => {
+  elemento.classList.remove(removeClass);
+  elemento.classList.add(addClass);
 };
 
 export const toggleClass = (element, toggleClass) => {
-    element.classList.toggle(toggleClass);
+  element.classList.toggle(toggleClass);
 };
 
 //Añadimos y quitamos loader con class
@@ -20,4 +20,4 @@ export const showQuotesForm = () => detailSection.style.display ='block';
 export const hideQuotesForm = () => detailSection.style.display = 'none';
 
 //forma 2
- //export const handleQuotesForm = diplay => detailSection.style.display=diplay;
+//export const handleQuotesForm = diplay => detailSection.style.display=diplay;
