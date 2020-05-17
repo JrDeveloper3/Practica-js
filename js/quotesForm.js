e9b0eb0666ef0c48072a2a53591e6bfd44ea6b71
Jorge Ruiz Martínez <jorgeruizdevelopers@gmail.com>
2020-05-17 19:38:53 +0200

    bug quotesForm

--- js/quotesForm.js
+++ js/quotesForm.js
@@ -1,7 +1,7 @@
 
 import api from './api.js';
 
-const QUOTES_API = "https://quotes-api-keepcoding.herokuapp.com/api/v1/";
+const QUOTES_API = 'https://quotes-api-keepcoding.herokuapp.com/api/v1/';
 
 const { createQuote} = api(QUOTES_API);
 
@@ -10,29 +10,30 @@ export const quoteTemplate = ({quote,date}) => `
         <p>${quote}</p>
         <span>${date}</span>
     </div>
-`
+`;
 
-const addQuoteListener = id => {
+//const addQuoteListener = id => {
 const quotesForm = document.querySelector('#quote-form');
 const quotesInput = document.querySelector('#quote');
 const quoteList = document.querySelector('#quoteList');
 
 quotesForm.addEventListener('submit', async (evt) =>{
-    evt.preventDefault();
-    try{
-        if(quotesInput.validity.valid){
-         //const id = window.location.pathname.split('/detail/')[1];
-         const response = await createQuote(id,quotesInput.value);
-         quoteList.innerHTML += quoteTemplate(response)
-        } else {
-            //funcion!!!!
-        }
-    }catch(err){
-        console.err(err);
+  evt.preventDefault();
+  try{
+    if(quotesInput.validity.valid){
+      const id = window.location.pathname.split('/detail/')[1];
+      const response = await createQuote(id,quotesInput.value);
+      quoteList.innerHTML += quoteTemplate(response);
+      quotesInput.value = '';
+    } else {
+    //funcion!!!!
     }
+  }catch(err){
+    console.err(err);
+  }
 });
 
-};
+//};
 
-export default addQuoteListener;
+//export default addQuoteListener;
 
--- js/routes.js
+++ js/routes.js
@@ -2,7 +2,7 @@ import { renderShowsDOM } from './shows.js';
 import  renderDetail from './detail.js';
 import { showFilter, hideFilter } from './navbar.js';
 import { showQuotesForm, hideQuotesForm } from './ui.js';
-import  addQuoteListener  from './quotesForm.js';
+//import  addQuoteListener  from './quotesForm.js';
 
 page('/',()=> {
   console.log('home page');
@@ -17,6 +17,6 @@ page('/detail/:id', ctx => {
   hideFilter(); // quita buscador
   showQuotesForm();
   renderDetail(id);
-  addQuoteListener(id);
+  //addQuoteListener(id);
 });
 page();
